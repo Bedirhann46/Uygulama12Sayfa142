a58c95e949f89f59256995a91b9b48c90ab8d838
bedirhann46 <bedirhannergiz46@gmail.com>
2024-03-12 12:03:12 +0300

    Proje dosyası ekle.

---
+++ Uygulama12Sayfa142/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Uygulama12Sayfa142
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnAraEkle = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.listeleSehirler = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSehirler = new System.Windows.Forms.TextBox();
+            this.labelDurum = new System.Windows.Forms.Label();
+            this.btnReverse = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(69, 89);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 0;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnAraEkle
+            // 
+            this.btnAraEkle.Location = new System.Drawing.Point(179, 88);
+            this.btnAraEkle.Name = "btnAraEkle";
+            this.btnAraEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnAraEkle.TabIndex = 1;
+            this.btnAraEkle.Text = "Araya Ekle";
+            this.btnAraEkle.UseVisualStyleBackColor = true;
+            this.btnAraEkle.Click += new System.EventHandler(this.btnAraEkle_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(295, 87);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(75, 23);
+            this.btnGuncelle.TabIndex = 2;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(400, 88);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 3;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(506, 88);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(75, 23);
+            this.btnAra.TabIndex = 4;
+            this.btnAra.Text = "Ara";
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // listeleSehirler
+            // 
+            this.listeleSehirler.FormattingEnabled = true;
+            this.listeleSehirler.Location = new System.Drawing.Point(631, 89);
+            this.listeleSehirler.Name = "listeleSehirler";
+            this.listeleSehirler.Size = new System.Drawing.Size(120, 160);
+            this.listeleSehirler.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Şehirler";
+            // 
+            // txtSehirler
+            // 
+            this.txtSehirler.Location = new System.Drawing.Point(179, 25);
+            this.txtSehirler.Name = "txtSehirler";
+            this.txtSehirler.Size = new System.Drawing.Size(100, 20);
+            this.txtSehirler.TabIndex = 7;
+            // 
+            // labelDurum
+            // 
+            this.labelDurum.AutoSize = true;
+            this.labelDurum.Font = new System.Drawing.Font("Noto Serif Cond", 21.75F, System.Drawing.FontStyle.Bold);
+            this.labelDurum.Location = new System.Drawing.Point(65, 183);
+            this.labelDurum.Name = "labelDurum";
+            this.labelDurum.Size = new System.Drawing.Size(108, 39);
+            this.labelDurum.TabIndex = 8;
+            this.labelDurum.Text = "Durum";
+            // 
+            // btnReverse
+            // 
+            this.btnReverse.Location = new System.Drawing.Point(69, 289);
+            this.btnReverse.Name = "btnReverse";
+            this.btnReverse.Size = new System.Drawing.Size(75, 23);
+            this.btnReverse.TabIndex = 9;
+            this.btnReverse.Text = "Reverse";
+            this.btnReverse.UseVisualStyleBackColor = true;
+            this.btnReverse.Click += new System.EventHandler(this.btnReverse_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(203, 288);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(75, 23);
+            this.btnSort.TabIndex = 10;
+            this.btnSort.Text = "Sort";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnReverse);
+            this.Controls.Add(this.labelDurum);
+            this.Controls.Add(this.txtSehirler);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listeleSehirler);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnAraEkle);
+            this.Controls.Add(this.btnEkle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnAraEkle;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.ListBox listeleSehirler;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSehirler;
+        private System.Windows.Forms.Label labelDurum;
+        private System.Windows.Forms.Button btnReverse;
+        private System.Windows.Forms.Button btnSort;
+    }
+}
+
---
+++ Uygulama12Sayfa142/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uygulama12Sayfa142
+{
+    public partial class Form1 : Form
+    {
+        ArrayList sehirler = new ArrayList();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            sehirler.Add(txtSehirler.Text);
+            Listele();
+            txtSehirler.Clear();
+
+        }
+        
+
+        private void btnAraEkle_Click(object sender, EventArgs e)
+        {
+            int indexNo = listeleSehirler.SelectedIndex;
+            sehirler.Insert(indexNo, txtSehirler.Text);
+            Listele();
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            int indexNo = listeleSehirler.SelectedIndex;
+            sehirler[indexNo] = txtSehirler.Text;
+            Listele();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            int indexNo = listeleSehirler.SelectedIndex;
+            sehirler.RemoveAt(indexNo);
+            Listele();
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            {
+                if (sehirler.Contains(txtSehirler.Text))
+                {
+                    labelDurum.Text = "Aranan Değer Bulundu.";
+                    listeleSehirler.SelectedIndex = sehirler.IndexOf(txtSehirler.Text);
+                }
+                else
+                {
+                    labelDurum.Text = "Aranan Değer Bulunamadı.";
+                }
+            }
+        }
+        private void Listele()
+        {
+            listeleSehirler.Items.Clear();
+            foreach (string sehir in sehirler)
+            {
+                listeleSehirler.Items.Add(sehir);
+            }
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            sehirler.Sort();
+            Listele();
+        }
+
+        private void btnReverse_Click(object sender, EventArgs e)
+        {
+            sehirler.Sort();
+            sehirler.Reverse();
+            Listele();
+        }
+    }
+}
